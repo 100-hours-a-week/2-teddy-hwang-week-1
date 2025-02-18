689cc55f9719e6976d6e301e95f2f0a425cadfab
junsik <deeptree97@gmail.com>
2025-02-19 03:19:00 +0900

    modified: 예약 정보에 좌석정보 추가

--- MovieBooking/src/model/Reservation.java
+++ MovieBooking/src/model/Reservation.java
@@ -2,21 +2,25 @@ package model;
 
 public class Reservation {
     private int reservationId;
+    private Showtime showtime;
     private Movie movie;
     private String seat;
 
-    public Reservation(int reservationId, Movie movie, String seat) {
+
+    public Reservation(int reservationId, Showtime showtime, Movie movie, String seat) {
         this.reservationId = reservationId;
+        this.showtime = showtime;
         this.movie = movie;
         this.seat = seat;
     }
 
     public int getReservationId() { return reservationId; }
+    public Showtime getShowtime() { return showtime; }
     public Movie getMovie() { return movie; }
     public String getSeat() { return seat; }
 
     @Override
     public String toString() {
-        return "🎟 예매번호: " + reservationId + " | 영화: " + movie.getTitle() + " | 좌석: " + seat;
+        return "🎟 예매번호: " + reservationId + " | 영화: " + movie.getTitle() + " | 상영관: " + showtime.getScreen().getName() + " | 상영시간: " + showtime.getTime() + " | 좌석: " + seat;
     }
 }
